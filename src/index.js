3d31a499cb881753e61da54dbc2f4f721c297c2c
CaptainCrocus <captaincrocus@gmail.com>
2019-01-01 21:10:52 +0400

    031 all module types

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import { top, bottom } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
 
-console.log(nav(), top, bottom, makeButton('My first button!'), makeColorStyle('mustard'));
+console.log(
+    nav(), 
+    top, 
+    bottom, 
+    makeButton('My first button!'), 
+    makeColorStyle('cyan')
+);
